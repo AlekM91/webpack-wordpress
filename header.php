32f052db15e73c7605ea826c391718adeda32b05
Aleksandar Mitrevski <aleksandarmitrevski91@gmail.com>
2022-09-03 17:44:03 +0200

    minor fixes

--- header.php
+++ header.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body class="<?php body_class(); ?>">
+<body <?php body_class(); ?>>
     
 <p>This is the header</p>
